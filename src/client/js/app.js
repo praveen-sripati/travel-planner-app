e02b6127d8da5572227431389cea08b61809e52a
Praveen <pvnsripati@gmail.com>
2020-01-11 21:58:21 +0530

    feat: Add comments to the project

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,7 @@
-// Personal API Key for OpenWeatherMap API
+//config file is used to store api secret keys
 import {config} from '../../../config';
+
+//Importing all SVG's of weather icons
 import clearDay from '../media/weather-icons/clear-day.svg';
 import clearNight from '../media/weather-icons/clear-night.svg';
 import cloudy from '../media/weather-icons/clear-day.svg';
@@ -14,17 +16,28 @@ import thunderstorm from '../media/weather-icons/thunderstorm.svg';
 import tornado from '../media/weather-icons/tornado.svg';
 import wind from '../media/weather-icons/wind.svg';
 
+//Global variables
+//geonames API Endpoint
 const baseUrl = "http://api.geonames.org/searchJSON?q=";
 const USER_NAME = "&username="+config.USER_NAME;
 const maxRows = "&maxRows=1";
+
+//DarkSky API Endpoint
 const darkSkyBaseURL = "https://api.darksky.net/forecast/";
 const darkSkyAPI_KEY = config.darkskyAPI_KEY;
+
+//Pixabay API Endpoint
 const pixabayBaseURL = "https://pixabay.com/api/?key=";
 const pixabayAPI_KEY = config.pixabayAPI_KEY;
 
+//Date months in 3 letters string form
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-/* Function called by event listener */
+/**
+ * @description Main function used to extract and display weather data
+ * @param {*} e
+ * @return none
+ */
 export function performAction(e) {
   const City = document.getElementById('city').value;
   const date = document.getElementById('date').value;
@@ -77,13 +90,26 @@ export function performAction(e) {
   })
 }
 
-/*Days Countdown*/
+/**
+ * @description Evaluate number of days away to travel journey
+ * @param {*} dateValue
+ * @param {*} currentDate
+ * @returns numbers of days away to travel
+ */
 function daysCountdown(dateValue, currentDate) {
   const oneDayInMs = 1000*60*60*24;
-  const diffInMs = dateValue.getTime() - currentDate.getTime();  return Math.floor(diffInMs/oneDayInMs);
+  const diffInMs = dateValue.getTime() - currentDate.getTime();
+  return Math.floor(diffInMs/oneDayInMs);
 }
 
-/* Function to GET Geonames Web API Data*/
+/**
+ * @description Function to GET Geonames Web API Data
+ * @param {*} baseUrl
+ * @param {*} City
+ * @param {*} maxRows
+ * @param {*} USER_NAME
+ * @returns json response
+ */
 const getCoordinates = async (baseUrl, City, maxRows, USER_NAME) => {
   const res = await fetch(baseUrl+City+maxRows+USER_NAME)
   try {
@@ -100,7 +126,14 @@ const getCoordinates = async (baseUrl, City, maxRows, USER_NAME) => {
   }
 }
 
-/*Function to Darksky Web API Data */
+/**
+ * @description Function to Darksky Web API Data
+ * @param {*} darkSkyBaseURL
+ * @param {*} darkSkyAPI_KEY
+ * @param {*} coordinates
+ * @param {*} time
+ * @returns json response
+ */
 const getWeather = async (darkSkyBaseURL, darkSkyAPI_KEY, coordinates,time) => {
   const apiURL = darkSkyBaseURL+darkSkyAPI_KEY+coordinates+time;
   const res = await fetch('http://localhost:8000/weather', {
@@ -126,6 +159,13 @@ const getWeather = async (darkSkyBaseURL, darkSkyAPI_KEY, coordinates,time) => {
   }
 }
 
+/**
+ * @description Function to Darksky Web API Data
+ * @param {*} pixabayBaseURL
+ * @param {*} pixabayAPI_KEY
+ * @param {*} query
+ * @returns json response
+ */
 const getImage = async (pixabayBaseURL, pixabayAPI_KEY, query) => {
   const res = await fetch(pixabayBaseURL+pixabayAPI_KEY+query)
   try {
@@ -142,7 +182,7 @@ const getImage = async (pixabayBaseURL, pixabayAPI_KEY, query) => {
   }
 }
 
-/* Function to POST data */
+/* Function to POST weather data */
 const postData = async (url='', data={}) => {
   const res = await fetch(url, {
     method: 'POST',
@@ -161,56 +201,69 @@ const postData = async (url='', data={}) => {
   }
 }
 
-/* Function to GET Project Data */
+/* Function to GET weather Data */
 const updateUI = async () => {
   const res = await fetch('/getProjectData');
   try {
     const projectData = await res.json();
     recentEntry(projectData);
-    // if(allData.length > 1) {
-    //   previousEntry(allData);
-    // }
   } catch(error) {
     console.log("error", error);
     alert("Something went wrong!");
   }
 }
 
+/**
+ * @description Update UI of the website
+ * @param {*} projectData
+ * @returns none
+ */
 function recentEntry(projectData) {
 
   console.log(projectData);
 
+  //Sets background color of trip-container
   document.getElementsByClassName('trip-container')[0].style.backgroundColor = "#E27429";
 
+  //Displays Weather details
+  //trip location
   document.getElementsByClassName('place')[0].innerHTML = `My trip to: ${projectData.city}, ${projectData.country}`;
 
+  //trip departing time
   document.getElementsByClassName('departing')[0].innerHTML = `Departing: ${projectData.depart}`;
 
+  //Number of days away to travel
   if (projectData.daysCount > 0) {
     document.getElementsByClassName('days-count')[0].innerHTML = `${projectData.city}, ${projectData.country} is ${projectData.daysCount} day(s) away`;
   } else {
     document.getElementsByClassName('days-count')[0].innerHTML = '';
   }
 
+  //typical weather title
   document.getElementsByClassName('typical-weather-title')[0].innerHTML = `Typical weather for then is:`;
 
+  //High temperature
   document.getElementsByClassName('temp-high')[0].innerHTML = `High: ${projectData.tempHigh} °C`;
 
+  //Low temperature
   document.getElementsByClassName('temp-low')[0].innerHTML = `Low: ${projectData.tempLow} °C`;
 
+  //summary
   if(projectData.summary !== undefined) {
     document.getElementsByClassName('summary')[0].innerHTML = projectData.summary;
   }
 
+  //Displays image of the location to travel
   if(projectData.imageURL !== undefined) {
     const image = document.getElementById('image-location')
     image.setAttribute('src',projectData.imageURL)
     image.setAttribute('width','100%');
     image.style.boxShadow = "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)"
-  } else {
+  } else { //Displays below text if imageURL is not provided by the Pixabay API
     document.getElementsByClassName('trip-image')[0].innerHTML = `image of the location`;
   }
 
+  //Displays weather icons (SVG's) if matches with Darksky API icon property
   const icon = document.getElementById('weather-icon');
   switch(projectData.icon) {
     case 'clear-day':
@@ -271,14 +324,4 @@ function recentEntry(projectData) {
         icon.removeAttribute('title');
       }
   }
-
 }
-
-
-// function previousEntry(allData) {
-//   document.getElementById('prevEntry').style.cssText = "margin-top:1rem; padding: 1rem;";
-//   document.getElementById('prevCity').innerHTML = allData[1].city;
-//   document.getElementById('prevDate').innerHTML = allData[1].date;
-//   document.getElementById('prevTemp').innerHTML = `${allData[1].temp}°C ${allData[1].condition}`;
-//   document.getElementById('prevContent').innerHTML =  "\""+allData[1].userResponse+"\"";
-// }
